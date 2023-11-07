0d4328698fa3eab4aad91ba66d89736006f3af5f
Atticus Blanco <atticusbrujx@gmail.com>
2023-11-07 00:04:03 -0800

    Part 1 >>>NONOPERATIONAL
    >>>MODIFICATIONS
    --CREATED ArrayProcessing.java
      -STUB constructor ArrayProcessing
      -STUB method getNumbers
      -STUB method processNumbers
      -STUB method printMenu
      -STUB method add
      -STUB method mult
      -STUB method print
      -STUB method reverse
      -STUB method again
    
    >>>DEFICITS
    --nothing works
    
    >>>BUGS
    --
    
    >>>THROWS
    --

---
+++ src/ArrayProcessing.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class ArrayProcessing {
+
+
+
+    public ArrayProcessing(){
+        return;
+    }
+
+    public void getNumbers(){
+        return;
+    }
+
+    public void processNumbers(){
+        return;
+    }
+
+    public void printMenu(){
+        return;
+    }
+
+    public void add(){
+        return;
+    }
+
+    public void mult(){
+        return;
+    }
+
+    public void print(){
+        return;
+    }
+
+    public void reverse(){
+        return;
+    }
+
+    public void again(){
+        return;
+    }
+}
