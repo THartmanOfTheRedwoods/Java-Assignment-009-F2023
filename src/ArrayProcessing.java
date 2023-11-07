54570d7af2c0519d4c6da2dec95cb02a5a9aec39
Atticus Blanco <atticusbrujx@gmail.com>
2023-11-07 12:23:20 -0800

    Part 1 >>>OPERATIONAL
    >>>MODIFICATIONS
    --ArrayProcessing.java
      -BUILT constructor ArrayProcessing
      -BUILT method getNumbers
      -BUILt method printMenu; structure copied from Lab 008
      -CREATED method main
      -BUILT method processNumbers; loop framework
    
    >>>DEFICITS
    --ArrayProcessing.java
      -BUILD method add
      -BUILD method mult
      -BUILD method print
      -BUILD method reverse
      -BUILD method again
    
    >>>BUGS
    --METHOD again(); exit program option continues the program.
    
    >>>THROWS
    --C:\Users\Gnomatic\IdeaProjects\Java-Assignment-009\src\ArrayProcessing.java
      Warning:(1, 1) Unused import statement
      Warning:(2, 1) Unused import statement
      Warning:(8, 19) Contents of array 'intArray' are written to, but never read
      Warning:(8, 19) Field 'intArray' may be 'final'
      Warning:(9, 21) Field 's' may be 'final'
      Warning:(42, 33) Variable 'repeatProgram' initializer 'Boolean.parseBoolean(null)' is redundant
      Warning:(42, 33) Result of 'Boolean.parseBoolean(null)' is always 'false'
      Warning:(46, 17) Switch statement can be replaced with enhanced 'switch'
      Warning:(77, 13) Local variable 'userOption' is redundant
      Warning:(86, 9) 'return' is unnecessary as the last statement in a 'void' method
      Warning:(93, 9) 'return' is unnecessary as the last statement in a 'void' method
      Warning:(100, 9) 'return' is unnecessary as the last statement in a 'void' method
      Warning:(107, 9) 'return' is unnecessary as the last statement in a 'void' method
      Warning:(116, 33) Result of 'Boolean.parseBoolean(null)' is always 'false'
      Warning:(123, 29) Variable 'userAgain' initializer '0' is redundant
      Warning:(126, 13) Switch statement can be replaced with enhanced 'switch'
      Warning:(133, 21) Variable is already assigned to this value

--- src/ArrayProcessing.java
+++ src/ArrayProcessing.java
@@ -7,13 +7,21 @@ public class ArrayProcessing {
 
     private int[] intArray;
     private Scanner s;
-    private static final String[] menu = {
+    private static final String[] menuProcess = {
             "Choose an option to perform:",
             ") Add numbers",
             ") Multiply numbers",
             ") Print numbers",
             ") Print number in reverse"
         };
+
+    private static final String[] menuAgain = {
+            "Would you like to choose another option?",
+            ") Choose another option",
+            ") Exit program"
+    };
+
+    private static final String errMessage = "Invalid Entry";
 
     public ArrayProcessing(){
         this.intArray = new int[5];
@@ -31,50 +39,104 @@ public class ArrayProcessing {
     }
 
     public void processNumbers(){
+        boolean repeatProgram = Boolean.parseBoolean(null);
         do{
-            switch (printMenu()) {
-                case 1:
-                    break;
-                case 2:
-                    break;
-                case 3:
-                    break;
-                case 4:
-                    break;
-                default:
-                    System.out.println("Invalid Entry");
-            }
-        } while (again());
+            boolean invalidInput = true;
+            do {
+                switch (printMenu()) {
+                    case 1:
+                        invalidInput = false;
+                        add();
+                        break;
+                    case 2:
+                        invalidInput = false;
+                        mult();
+                        break;
+                    case 3:
+                        invalidInput = false;
+                        print();
+                        break;
+                    case 4:
+                        reverse();
+                        invalidInput = false;
+                        break;
+                    default:
+                        System.out.println(errMessage);
+                }
+            } while (invalidInput);
+            repeatProgram = again();
+        } while (repeatProgram);
 
     }
 
     public int printMenu(){
-        System.out.println(menu[0]);
-        for (int i = 1; i < menu.length; i++) {
-            System.out.printf("%d: %s%n", i, menu[i]);
+        System.out.println(menuProcess[0]);
+        for (int i = 1; i < menuProcess.length; i++) {
+            System.out.printf("%d: %s%n", i, menuProcess[i]);
         }
         int userOption = Integer.parseInt(s.nextLine());        //write check on input
         return userOption;
     }
 
+
+//    Create a method named, add, with a return type of void and no parameters that does the following:
+//    Uses a for loop and the array length field to sum the numbers in the array and print the final sum, with the
+//    appropriate label, only once to the terminal window.
     public void add(){
         return;
     }
 
+//    Create a method named, mult, with a return type of void and no parameters that does the following:
+//    Uses a for loop and the array length field to multiply the numbers in the array together and print the final
+//    product, with the appropriate label, only once to the terminal window
     public void mult(){
         return;
     }
 
+//    Create a method named, print, with a return type of void and no parameters that does the following:
+//    Uses a for loop and the array length field to print each number, with the appropriate label, in the array to
+//    the terminal window.
     public void print(){
         return;
     }
 
+//    Create a method named, reverse, with a return type of void and no parameters that does the following:
+//    Uses a for loop and the array length field to print each number in the array IN REVERSE ORDER, with the
+//    appropriate label, to the terminal window.
     public void reverse(){
         return;
     }
 
+
+//    Create a method named, again, with a return type of boolean and no parameters that does the following:
+//    Prompts the user to enter the integer 1 to choose another option or 2 to exit.
+//    Grabs an int from the terminal using the Scanner object.
+//    Tests the number entered and returns true if the int is a 1 and false if it is something else.
     public boolean again(){
-        return false;
+        boolean repeatProgram = Boolean.parseBoolean(null);
+        boolean invalidInput = true;
+        do {
+            System.out.println(menuAgain[0]);
+            for (int i = 1; i < menuAgain.length; i++) {
+                System.out.printf("%d: %s%n", i, menuAgain[i]);
+            }
+            int userAgain = 0;
+                    userAgain = Integer.parseInt(s.nextLine());        //write check on input
+
+            switch (userAgain) {
+                case 1:
+                    invalidInput = false;
+                    repeatProgram = true;
+                    break;
+                case 2:
+                    invalidInput= false;
+                    repeatProgram = false;
+                    break;
+                default:
+                    System.out.println(errMessage);
+            }
+        } while (invalidInput);
+        return repeatProgram;
     }
 
     public static void main(String[] args){
